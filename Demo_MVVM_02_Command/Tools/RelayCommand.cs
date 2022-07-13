4bd7f46ff40af7308f12183d5d9306c5e9f67f1f
Aurélien <aurelien.strimelle@bstorm.be>
2022-07-13 16:47:21 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_MVVM_02_Command/Tools/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MVVM_02_Command.Tools
+{
+    public class RelayCommand : IRelayCommand
+    {
+        #region Event
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+
+        private readonly Action _Execute;
+        private readonly Func<bool> _CanExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            if (execute is null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _Execute();
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            // return _CanExecute?.Invoke() ?? true;
+            return _CanExecute is null ? true : _CanExecute();
+        }
+    }
+}
---
+++ Demo_MVVM_02_Command/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Demo_MVVM_02_Command.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MVVM_02_Command.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        #region Event
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Field
+        private int _Count;
+        #endregion
+
+        #region Props
+        public int Count
+        {
+            get { return _Count; }
+            set 
+            { 
+                _Count = value;
+                RaisePropertyChanged();
+
+                CommandPlus?.RaiseCanExecuteChanged();
+                CommandMinus?.RaiseCanExecuteChanged();
+            }
+        }
+
+        public IRelayCommand CommandPlus { get; set; }
+        public IRelayCommand CommandMinus { get; set; }
+        public IRelayCommand CommandReset { get; set; }
+        #endregion
+
+        #region Ctor
+        public MainViewModel()
+        {
+            Count = 20;
+
+            //CommandPlus = new RelayCommand(
+            //    () => Count++,
+            //    () => Count < 10
+            //) ;
+            CommandPlus = new RelayCommand(IncrCount, IncrCheck);
+            CommandMinus = new RelayCommand(DecrCount, DecrCheck);
+            CommandReset = new RelayCommand(ResetCount);
+        }
+        #endregion
+
+        #region Méthode
+        private void IncrCount()
+        {
+            Count++;
+            //CommandPlus.RaiseCanExecuteChanged();
+            //CommandMinus.RaiseCanExecuteChanged();
+        }
+        private bool IncrCheck()
+        {
+            return Count < 10;
+        }
+
+        private void DecrCount()
+        {
+            Count--;
+            //CommandPlus.RaiseCanExecuteChanged();
+            //CommandMinus.RaiseCanExecuteChanged();
+        }
+        private bool DecrCheck()
+        {
+            return Count > -10;
+        }
+
+        private void ResetCount()
+        {
+            Count = 0;
+        }
+        #endregion
+    }
+}
